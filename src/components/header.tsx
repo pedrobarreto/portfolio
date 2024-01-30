08cea7640f630aed6e3488ac6cf5634e03935b8a
Pedro Barreto <pbdeoliveira@gmail.com>
2024-01-30 16:58:59 -0300

    feat: responsive menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,10 @@
-// Header.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import { Link as ScrollLink } from 'react-scroll';
 import { cn } from '@/lib/utils';
 import { useTheme } from '@/contexts/theme-context';
-import { MoonIcon, SunIcon } from 'lucide-react';
-import { useTabContext } from '@/contexts/tab-context'
+import { MenuIcon, MoonIcon, SunIcon } from 'lucide-react';
+import { useTabContext } from '@/contexts/tab-context';
+import { useMediaQuery } from 'react-responsive';
 
 const sections = [
   { key: 'aboutMe', label: 'About Me' },
@@ -14,17 +14,19 @@ const sections = [
   { key: 'tabs', label: 'Articles' },
 ];
 
-const HeaderButton: React.FC<{ label: string, scrollId: string }> = ({ label, scrollId }) => {
+const HeaderButton: React.FC<{ label: string; scrollId: string; onClick: () => void }> = ({ label, scrollId, onClick }) => {
   const { selectedTab, setSelectedTab } = useTabContext();
+
+  const handleClick = () => {
+    setSelectedTab(label.toLowerCase());
+    onClick();
+  };
 
   return (
     <ScrollLink to={scrollId} spy={true} smooth={true} offset={-70} duration={500}>
       <button
-        className={cn(
-          'text-sm md:text-sm',
-          selectedTab === label.toLowerCase() ? 'text-blue-500' : ''
-        )}
-        onClick={() => setSelectedTab(label.toLowerCase())}
+        className={cn('text-sm md:text-sm', selectedTab === label.toLowerCase() ? 'text-blue-500' : '')}
+        onClick={handleClick}
       >
         {label}
       </button>
@@ -42,18 +44,47 @@ const DarkModeButton: React.FC = () => {
   );
 };
 
-export const Header: React.FC = () => (
-  <>
-  <header className="w-full flex justify-between items-center h-9 border-b z-50 sticky top-0 px-4 sm:px-0 md:px-0 lg:px-0">
-    <div className="w-full max-w-2xl mx-auto flex justify-between items-center">
-      <div className="flex items-center space-x-4">
-        {sections.map((section) => (
-          <HeaderButton key={section.label} label={section.label} scrollId={section.key} />
-        ))}
-      </div>
-      <DarkModeButton />
-    </div>
-  </header>
-  <div className="h-0.5 w-full border-transparent bg-gradient-to-r from-pink-500 via-blue-500 to-green-400"></div>
-  </>
-);
+export const Header: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const { darkMode, toggleDarkMode } = useTheme();
+
+  const handleMenuButtonClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleMenuItemClick = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <>
+      <header className="w-full flex justify-between items-center h-9 border-b z-50 sticky top-0 px-4 sm:px-0 md:px-0 lg:px-0">
+        <div className="w-full max-w-2xl mx-auto flex justify-between items-center">
+          <div className="hidden md:flex items-center space-x-4">
+            {sections.map((section) => (
+              <HeaderButton key={section.label} label={section.label} scrollId={section.key} onClick={handleMenuItemClick} />
+            ))}
+          </div>
+          <div className="md:hidden flex items-center space-x-4">
+            <button className="text-xl" onClick={handleMenuButtonClick}>
+              <MenuIcon size={20} />
+            </button>
+          </div>
+          <DarkModeButton />
+        </div>
+      </header>
+      {menuOpen && (
+        <div className={`w-full sticky top-9 bg-${darkMode ? 'black' : 'white' } border-gray-200 shadow-md z-50`} style={{ marginTop: isMobile ? '-50px' : '0' }}>
+          <div className="max-w-2xl mx-auto flex justify-between items-center p-4">
+            {sections.map((section) => (
+              <HeaderButton key={section.label} label={section.label} scrollId={section.key} onClick={handleMenuItemClick} />
+            ))}
+          </div>
+          <div className="h-0.5 w-full border-transparent bg-gradient-to-r from-pink-500 via-blue-500 to-green-400"></div>
+        </div>
+      )}
+      <div className="h-0.5 w-full border-transparent bg-gradient-to-r from-pink-500 via-blue-500 to-green-400"></div>
+    </>
+  );
+};
