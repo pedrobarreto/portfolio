b874aeaaaa278df6e966a5b821913544d9e6b311
Pedro Barreto <pbdeoliveira@gmail.com>
2024-06-10 18:57:43 -0300

    fix: icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ import React from "react";
 export const metadata: Metadata = {
   title: `${RESUME_DATA.name} | ${RESUME_DATA.about}`,
   description: RESUME_DATA.summary,
+  icons: {
+    icon: '/icon.png',
+  },
 };
 
 const roboto = Roboto({
