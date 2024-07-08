bd172b9de180ce1fc828c5e3a1ab5340bc766268
Pedro Barreto <pbdeoliveira@gmail.com>
2024-07-08 09:53:24 -0300

    feat: refactor requests

--- src/services/requests.ts
+++ src/services/requests.ts
@@ -1,21 +1,35 @@
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
-const githubApi: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_GITHUB_API_BASE_URL,
+const backendBaseUrl = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
+
+const backendApi: AxiosInstance = axios.create({
+  baseURL: backendBaseUrl,
 });
 
 export const requestProjects = async (endpoint: string, body?: any): Promise<any> => {
-  const { data }: AxiosResponse = await githubApi.get(endpoint, body);
+  const { data }: AxiosResponse = await backendApi.post(endpoint, body);
   return data;
 };
 
-const devToApiBaseUrl = process.env.NEXT_PUBLIC_DEV_TO_API_BASE_URL;
-
-export const devToApi: AxiosInstance = axios.create({
-  baseURL: devToApiBaseUrl,
-});
-
 export const requestPosts = async (endpoint: string, body?: any): Promise<any> => {
-  const { data }: AxiosResponse = await devToApi.post(endpoint, body);
+  const { data }: AxiosResponse = await backendApi.post(endpoint, body);
+  return data;
+};
+
+interface Video {
+  title: string;
+  description: string;
+  tags: string[];
+  link: string;
+  imageUrl: string;
+}
+
+export const requestVideos = async (endpoint: string, body?: { pagination: number; language: string }): Promise<Video[]> => {
+  const { data }: AxiosResponse<Video[]> = await backendApi.post(endpoint, body);
+
+  if (!Array.isArray(data)) {
+    throw new Error('Unexpected response format');
+  }
+
   return data;
 };
