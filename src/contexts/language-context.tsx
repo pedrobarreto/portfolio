b6e9dc802c9a0c4f30380dca1bcb9093828408cb
Pedro Barreto <pbdeoliveira@gmail.com>
2024-07-08 09:54:13 -0300

    feat: language context & provider

---
+++ src/contexts/language-context.tsx
@@ -0,0 +1,35 @@
+import React, { createContext, useState, useContext, useEffect } from 'react';
+
+type Language = 'en' | 'pt';
+
+interface LanguageContextProps {
+  language: Language;
+  toggleLanguage: () => void;
+}
+
+const LanguageContext = createContext<LanguageContextProps | undefined>(undefined);
+
+export const useLanguage = () => {
+  const context = useContext(LanguageContext);
+  if (!context) {
+    throw new Error('useLanguage must be used within a LanguageProvider');
+  }
+  return context;
+};
+
+export const LanguageProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [language, setLanguage] = useState<Language>(() => {
+    const userLanguage = navigator.language.split('-')[0];
+    return userLanguage === 'en' ? 'en' : 'pt';
+  });
+
+  const toggleLanguage = () => {
+    setLanguage((prevLanguage) => (prevLanguage === 'en' ? 'pt' : 'en'));
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, toggleLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -2,14 +2,17 @@
 
 import { ThemeProvider } from "@/contexts/theme-context";
 import { TabContextProvider } from "@/contexts/tab-context";
+import { LanguageProvider } from "@/contexts/language-context";
 import { ReactNode } from "react";
 
-export function Providers({ children } : { children: ReactNode} ) {
+export function Providers({ children } : { children: ReactNode }) {
   return (
     <ThemeProvider>
-      <TabContextProvider>
-    {children}
-    </TabContextProvider>
+      <LanguageProvider>
+        <TabContextProvider>
+          {children}
+        </TabContextProvider>
+      </LanguageProvider>
     </ThemeProvider>
   );
 }
